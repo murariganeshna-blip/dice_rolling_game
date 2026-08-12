b92175d75e21345eddf7371bab2df3ab460780dd
murariganeshna-blip <murariganeshna@gmail.com>
2026-08-12 08:41:34 +0530

    Refactor dice rolling input prompts and count

--- new.py
+++ new.py
@@ -1,16 +1,25 @@
 import random
 
+count = [0]
+
 while True:
-    dice=input("Roll the dice ? (y/n):")
+    dice=input("Roll the dice? (y/n):")
     if dice.lower()=="y":
-        num=int(input("How many dice to be rolled?:"))
+        num=int(input("How many dice ?:"))
         rolls=[]
         for i in range(num):
             rolls.append(random.randint(1,6))
+        count[0]+= 1
         print(rolls)
+        print(f"Num of rolls: {count[0]}")
+
     elif dice.lower()=="n":
         print("Thanks for playing !")
         break
     else:
-       print("Invalid choice!")
+        print("Invalid choice!")
+
+
+
+
 
